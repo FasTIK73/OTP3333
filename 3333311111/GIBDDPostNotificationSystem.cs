181f277137618cc16049259325fcb48f16d132bc
Antonn <gugle23v@gmail.com>
2025-11-19 12:17:05 +0400

    task_1_1

--- 3333311111/GIBDDPostNotificationSystem.cs
+++ 3333311111/GIBDDPostNotificationSystem.cs
@@ -1,25 +1,13 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ref_2;
 
-namespace _3333311111
+public class GIBDDPostNotificationSystem
 {
-    internal class GIBDDPostNotificationSystem
+    public static void NotifyPosts(string message)
     {
+        Console.WriteLine("Сообщение для всех постов ГИБДД: {0}", message);
     }
-}
-*/
-namespace ref_2
-{
-    public class GIBDDPostNotificationSystem
-    {
-        public static void NotifyPosts(string message)
-        {
-            Console.WriteLine("Сообщение для всех постов ГИБДД: {0}", message);
-        }
-        public string SendMessageToAllPosts(
+
+    public string SendMessageToAllPosts(
         int postId,
         string location,
         bool isEmergency,
@@ -27,39 +15,67 @@ namespace ref_2
         List<string> recipientsList,
         string senderName,
         string additionalInfo = null)
+    {
+        if (!CanSendMessage(isEmergency, recipientsList))
+            return GetErrorMessage(isEmergency, recipientsList);
+
+        SendMessagesToRecipients(postId, location, senderName, recipientsList);
+        return "Сообщение успешно доставлено";
+    }
+
+    private bool CanSendMessage(bool isEmergency, List<string> recipientsList)
+    {
+        return !isEmergency && recipientsList.Count > 0 ||
+               isEmergency && recipientsList.Count > 0;
+    }
+
+    private string GetErrorMessage(bool isEmergency, List<string> recipientsList)
+    {
+        if (isEmergency && recipientsList.Count <= 0)
+            return "Отсутствуют получатели для экстренного уведомления!";
+
+        return "Сообщение успешно отправлено.";
+    }
+
+    private void SendMessagesToRecipients(int postId, string location, string senderName, List<string> recipientsList)
+    {
+        foreach (var recipient in recipientsList)
         {
-            if (!isEmergency && recipientsList.Count > 0)
-                return $"Сообщение успешно отправлено.";
-            else if (isEmergency && recipientsList.Count <= 0)
-                return $"Отсутствуют получатели для экстренного уведомления!";
-            foreach (var recipient in recipientsList)
-            {
-                NotifyPosts($"Сообщение от поста №{postId}, местоположение: {location}. Отправитель: {senderName}");
-            }
-            return "Сообщение успешно доставлено";
+            SendMessageToRecipient(postId, location, senderName, recipient);
         }
-        public void HandleTrafficViolation(int violationCode)
+    }
+
+    private void SendMessageToRecipient(int postId, string location, string senderName, string recipient)
+    {
+        NotifyPosts($"Сообщение от поста №{postId}, местоположение: {location}. Отправитель: {senderName}");
+    }
+
+    public void HandleTrafficViolation(int violationCode)
+    {
+        string violationDescription = GetViolationDescription(violationCode);
+        Console.WriteLine(violationDescription);
+    }
+
+    private string GetViolationDescription(int violationCode)
+    {
+        return violationCode switch
         {
-            switch (violationCode)
-            {
-                case 1:
-                    Console.WriteLine("Превышение скорости");
-                    break;
-                case 2:
-                    Console.WriteLine("Проезд на красный свет");
-                    break;
-                default:
-                    Console.WriteLine("Неизвестное нарушение");
-                    break;
-            }
-        }
-        public void SendSpecialAlert(int alertLevel, string region, string[]
-       affectedAreas)
+            1 => "Превышение скорости",
+            2 => "Проезд на красный свет",
+            _ => "Неизвестное нарушение"
+        };
+    }
+
+    public void SendSpecialAlert(int alertLevel, string region, string[] affectedAreas)
+    {
+        foreach (var area in affectedAreas)
         {
-            for (int i = 0; i < affectedAreas.Length; i++)
-            {
-                NotifyPosts($"Спецуведомление уровня {alertLevel}: регион {region}, затронута область {affectedAreas[i]}");
-            }
+            SendAlertToArea(alertLevel, region, area);
         }
+    }
+
+    private void SendAlertToArea(int alertLevel, string region, string area)
+    {
+        NotifyPosts($"Спецуведомление уровня {alertLevel}: регион {region}, затронута область {area}");
     }
 }
